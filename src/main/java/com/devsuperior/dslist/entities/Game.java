85482ec7e9f0e21c075c84910f50636e7b9469fd
FernandoDeAquino <fg_aquino@yahoo.com.br>
2025-08-23 20:05:15 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Table;
 @Table(name = "tb_game")
 public class Game {
 	@Id
-	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	
